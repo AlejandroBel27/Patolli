77dc5fe74f7ab9808e4ac70909662114caca4187
AlejandroBel27 <ruben.saldana216100@potros.itson.edu.mx>
2024-10-30 02:25:04 -0700

    cracion de capa negocio falta terminar

---
+++ patolli-negocio/src/main/java/mx/itson/patolli/negocio/PatolliNegocio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mx.itson.patolli.negocio;
+
+/**
+ *
+ * @author cocob
+ */
+public class PatolliNegocio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
