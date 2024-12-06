eca5212b58dc6c7567e78ef4f5032f2ad95b0c95
linmoreno <linda.moreno216690@potros.itson.edu.mx>
2024-12-06 05:07:42 -0700

    mod

--- tcpComun/src/main/java/com/tcp/comun/ClientThread.java
+++ tcpComun/src/main/java/com/tcp/comun/ClientThread.java
@@ -3,11 +3,31 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.tcp.comun;
+import logins.IchatLogger;
+import logins.LogFactory;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.net.Socket;
+import entidades.Jugador;
+import java.io.IOException;
 
 /**
  *
  * @author gilbert
  */
-public class ClientThread {
+public class ClientThread extends Observable implements Runnable,Serializable{
+
+    
+    
+    @Override
+    public void notifyObservers(Object obj) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void run() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     
 }
